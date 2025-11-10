e9d02d420487500f4a608d1dbad006149d806bdd
Wil <98198668+Wilaz@users.noreply.github.com>
2025-11-09 22:07:30 -0500

    Improvements

--- src/QNAPControl/__init__.py
+++ src/QNAPControl/__init__.py
@@ -5,6 +5,7 @@ from threading import Thread
 
 PREAMBLES = [0x53, 0x83]
 
+
 class Response(Enum):
     ID = 0x01
     BUTTON = 0x05
@@ -13,10 +14,12 @@ class Response(Enum):
     NACK = 0xFB
     RACK = 0xAA
 
+
 class Key(Enum):
-    UP   = bytes([0x00, 0x01])
+    UP = bytes([0x00, 0x01])
     DOWN = bytes([0x00, 0x02])
     BOTH = bytes([0x00, 0x03])
+
 
 class Commands(Enum):
     BASE = 0x4D
@@ -28,39 +31,50 @@ class Commands(Enum):
     BACKLIGHT = 0x5E
     RESET = 0xFF
 
+
 class QnapLCD:
-    def __init__(self, handler:Callable[[Response, int|Key|bytes|None], None], port:str='/dev/ttyS1', speed:int=1200, rows:int=2, columns:int=16):
+    def __init__(
+        self,
+        handler: Callable[[Response, int | Key | bytes | None], None] | None,
+        port: str = "/dev/ttyS1",
+        speed: int = 1200,
+        rows: int = 2,
+        columns: int = 16,
+    ):
         self.serial: Serial = Serial(port, speed)
-        self.columns:int = columns
-        self.rows:int = rows
+        self.columns: int = columns
+        self.rows: int = rows
 
-        self.handler:Callable[[Response, int|Key|bytes|None], None] = handler
-        self.reader: Thread = Thread(target=self.handle_response)
-        self.reader.start()
+        if handler:
+            self.handler: Callable[[Response, int | Key | bytes | None], None] = handler
+            self.reader: Thread = Thread(target=self._handle_response)
+            self.reader.start()
 
-    def handle_response(self):
+    def _handle_response(self):
         while True:
             if self.serial.read()[0] in PREAMBLES:
                 match self.serial.read():
                     case Response.ID.value:
                         self.handler(Response.ID, int.from_bytes(self.serial.read(2)))
-            
+
                     case Response.BUTTON.value:
                         self.handler(Response.BUTTON, Key(self.serial.read(2)))
-            
+
                     case Response.PROTOCOL.value:
-                        self.handler(Response.PROTOCOL, int.from_bytes(self.serial.read(2)))
-            
+                        self.handler(
+                            Response.PROTOCOL, int.from_bytes(self.serial.read(2))
+                        )
+
                     case Response.RACK.value:
                         self.handler(Response.RACK, None)
-            
+
                     case Response.ACK.value:
                         self.handler(Response.ACK, None)
-            
+
                     case Response.NACK.value:
                         result = self.serial.read()
                         self.handler(Response.NACK, result)
-            
+
                     case _:
                         raise Exception
 
@@ -80,9 +94,20 @@ class QnapLCD:
         _ = self.serial.write(bytes([Commands.BASE.value, Commands.BUTTONS.value]))
 
     def backlight(self, on: bool = True):
-        _ = self.serial.write(bytes([Commands.BASE.value, Commands.BACKLIGHT.value, 0x01 if on else 0x00]))
+        _ = self.serial.write(
+            bytes([Commands.BASE.value, Commands.BACKLIGHT.value, 0x01 if on else 0x00])
+        )
 
     def write(self, msg: str, row: int = 1):
-            msg = msg[:self.columns]
-            _ = self.serial.write(bytes([Commands.BASE.value, Commands.DISPLAY_CHAR.value, (row - 1) % self.rows, len(msg)]))
-            _ = self.serial.write(msg.encode('utf-8'))
+        msg = msg[: self.columns]
+        _ = self.serial.write(
+            bytes(
+                [
+                    Commands.BASE.value,
+                    Commands.DISPLAY_CHAR.value,
+                    (row - 1) % self.rows,
+                    len(msg),
+                ]
+            )
+        )
+        _ = self.serial.write(msg.encode("utf-8"))
